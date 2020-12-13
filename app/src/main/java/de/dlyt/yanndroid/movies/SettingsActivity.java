58631b9d3341b353992bf91e20e0bb8f7455833b
Yanndroid <yann.ritzert@yahoo.fr>
2020-12-13 16:17:33 +0100

    Version 1.3.4

--- app/src/main/java/de/dlyt/yanndroid/movies/SettingsActivity.java
+++ app/src/main/java/de/dlyt/yanndroid/movies/SettingsActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.SharedPreferences;
 import android.content.res.Configuration;
 import android.content.res.Resources;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
